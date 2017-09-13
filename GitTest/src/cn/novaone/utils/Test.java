538831e4b2811f5c46587da4a4b09fb36ffc19d6
ycx435 <ycx435163.com>
2017-09-13 10:21:21 +0800

    fix bug123

--- GitTest/src/cn/novaone/utils/Test.java
+++ GitTest/src/cn/novaone/utils/Test.java
@@ -4,8 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("==hello.");
-		System.out.println("==hello2.");
+		System.out.println("==hello3.");
 	}
 
 }
